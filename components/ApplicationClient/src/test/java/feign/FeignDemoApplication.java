e8c8e056deda3ad9ed5bf1ca3244ff309760572a
lianshufeng <251708339@qq.com>
2021-06-03 21:42:20 +0800

    submit

---
+++ components/ApplicationClient/src/test/java/feign/FeignDemoApplication.java
@@ -0,0 +1,30 @@
+package feign;
+
+import com.fast.dev.acenter.annotation.EnableApplicationClient;
+import com.github.microservice.core.boot.ApplicationBootSuper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * 测试代码的入口
+ */
+
+//需要使用此注解，且配置 application.properties
+
+
+@ComponentScan("feign")
+@EnableFeignClients //必要
+@EnableApplicationClient
+public class FeignDemoApplication extends ApplicationBootSuper {
+
+
+    public static void main(String[] args) {
+        ApplicationContext ac = SpringApplication.run(FeignDemoApplication.class, args);
+
+
+    }
+
+
+}
